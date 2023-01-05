50027bc3da6cd561435fe9a4b4657ce51b026a1d
Nicholas Cross <nicholas@nacrossweb.com>
2023-01-06 07:51:35 +1100

    Initial Commit

---
+++ OpenAI/OpenAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenAIApp.swift
+//  OpenAI
+//
+//  Created by Nicholas Cross on 6/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct OpenAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
